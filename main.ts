6cff11e2112e86b271d8ee87228e4fff06562d92
sakwensivie <63986400+sakwensivie@users.noreply.github.com>
2022-12-11 08:23:14 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Yes)
+})
